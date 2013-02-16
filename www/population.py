dcaf08b1efe42254ae774c13b94152277e47fecd
Ryan Kanno <ryankanno@localkinegrinds.com>
2013-02-16 08:40:53 -0500

    Fixed population.py

--- www/population.py
+++ www/population.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from flask import Blueprint
-from flask import jsonify
+from flask import render_template
 
 app = Blueprint('population', __name__, url_prefix='/population')
 
 
 @app.route('/', methods=['GET'])
 def population():
+    return render_template('population.html')
 
 # vim: filetype=python
